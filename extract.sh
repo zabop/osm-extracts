b3870404dc89cb4b0073df733b72ebb440764ee0
zabop <szabopal96@gmail.com>
2025-05-03 16:17:41 +0200

    try upload result

--- extract.sh
+++ extract.sh
@@ -6,3 +6,5 @@ set -o pipefail
 
 curl https://download.geofabrik.de/europe/liechtenstein-latest.osm.pbf --output latest.osm.pbf
 ogr2ogr latest.gpkg latest.osm.pbf
+
+aws s3 cp latest.gpkg s3://osm-extracts/latest.gpkg
