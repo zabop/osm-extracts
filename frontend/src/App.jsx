4d943cbc8af819bbedceebc3f06fd7cc4a3b7648
zabop <szabopal96@gmail.com>
2025-05-04 17:49:51 +0200

    include actual links

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,23 @@
 import React, { useState, useEffect } from "react";
 
 const regions = [
-  { name: "Finland", path: "finland" },
-  { name: "Iceland", path: "iceland" },
-  { name: "Norway", path: "norway" },
-  { name: "Sweden", path: "sweden" },
+  { name: "Finland", path: "europe/finland" },
+  { name: "Iceland", path: "europe/iceland" },
+  { name: "Norway", path: "europe/norway" },
+  { name: "Sweden", path: "europe/sweden" },
 ];
 
 const extracts = [
-  { name: "Power", fname: "power.zip" },
-  { name: "Protected areas", fname: "protected_areas.zip" },
-  { name: "Administrative divisions", fname: "admin_divisions.zip" },
+  { name: "Power", fname: "power.gpkg" },
+  { name: "Protected areas", fname: "protected_areas.gpkg" },
+  { name: "Administrative divisions", fname: "admin_divisions.gpkg" },
 ];
 
 function CountryLinks({ region }) {
   const [lastUpdated, setLastUpdated] = useState(null);
 
   useEffect(() => {
-    const metadataUrl = `https://ukzckrzlamlgsschrwgd.supabase.co/storage/v1/object/public/yosmgm-testing0/europe/${region.path}/power.metadata.json`;
+    const metadataUrl = `https://ukzckrzlamlgsschrwgd.supabase.co/storage/v1/object/public/yosmgm-testing0/${region.path}/power.metadata.json`;
 
     const fetchMetadata = async () => {
       const response = await fetch(metadataUrl);
@@ -35,7 +35,11 @@ function CountryLinks({ region }) {
       <ul>
         {extracts.map((extract) => (
           <li key={`${region.path}/${extract.fname}`}>
-            <a href={`${region.path}/${extract.fname}`}>{extract.name}</a>
+            <a
+              href={`https://ukzckrzlamlgsschrwgd.supabase.co/storage/v1/object/public/yosmgm-testing0/${region.path}/${extract.fname}`}
+            >
+              {extract.name}
+            </a>
           </li>
         ))}
       </ul>
