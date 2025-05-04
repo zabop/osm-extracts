ca2b1a81e17b14ea508d22c44082b379e5e1c36d
zabop <szabopal96@gmail.com>
2025-05-04 18:54:35 +0200

    only include layers which matter

--- processing/extract.sh
+++ processing/extract.sh
@@ -7,11 +7,12 @@ set -o pipefail
 region=$1
 filter=$2
 dst=$3
+shift 3 # $@ now starts from $4
 
 curl "https://download.geofabrik.de/${region}-latest.osm.pbf" --output latest.osm.pbf
 osmium tags-filter -o filtered.osm.pbf latest.osm.pbf "${filter}"
 
 echo "Writing ${region%%/*}_${region##*/}_${dst}.gpkg" 
-ogr2ogr "${region%%/*}_${region##*/}_${dst}.gpkg" filtered.osm.pbf
+ogr2ogr "${region%%/*}_${region##*/}_${dst}.gpkg" filtered.osm.pbf "$@"
 
 python3 upload.py "${region}" "${dst}"
