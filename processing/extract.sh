295d1f13bbdf37629313d5a7e9f3203605603ed2
zabop <szabopal96@gmail.com>
2025-05-04 18:20:52 +0200

    more verbose

--- processing/extract.sh
+++ processing/extract.sh
@@ -10,6 +10,8 @@ dst=$3
 
 curl "https://download.geofabrik.de/${region}-latest.osm.pbf" --output latest.osm.pbf
 osmium tags-filter -o filtered.osm.pbf latest.osm.pbf "${filter}"
-ogr2ogr "${region}_${dst}".gpkg filtered.osm.pbf
+
+echo "Writing ${region}_${dst}.gpkg" 
+ogr2ogr "${region}_${dst}.gpkg" filtered.osm.pbf
 
 python3 upload.py "${region} ${dst}"
