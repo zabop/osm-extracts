9793771167c3cff3fa32496afcb5736ec036521f
zabop <szabopal96@gmail.com>
2025-05-04 19:58:22 +0200

    wording

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -69,7 +69,7 @@ function App() {
       <div>
         Not quite what you want? Email zabop.github.io.uncanny066@passfwd.com.
       </div>
-      <div>Files are daily.</div>
+      <div>Extracts are updated daily.</div>
       {regions.map((region) => (
         <CountryLinks key={region.path} region={region} />
       ))}
