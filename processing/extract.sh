01f1646b09e0e15dc2e6e19e787b5cfdde6fc7bc
zabop <szabopal96@gmail.com>
2025-05-04 17:58:48 +0200

    more complete filter controll

--- processing/extract.sh
+++ processing/extract.sh
@@ -9,7 +9,7 @@ filter=$2
 dst=$3
 
 curl "https://download.geofabrik.de/${region}-latest.osm.pbf" --output latest.osm.pbf
-osmium tags-filter -o filtered.osm.pbf latest.osm.pbf "nw/${filter}"
+osmium tags-filter -o filtered.osm.pbf latest.osm.pbf "${filter}"
 ogr2ogr dst.gpkg filtered.osm.pbf
 
 python3 upload.py "${region}/${dst}"
